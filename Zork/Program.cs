ddcd6aec4a76bc47010e046b0fdaf50e0caeb5a0
TealTurken <67345081+TealTurken@users.noreply.github.com>
2021-09-06 11:28:28 -0400

    Zork 1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,10 +8,37 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
+            Commands command = Commands.UNKNOWN; // assign command a value so it can run the initial comparison
+            while (command != Commands.QUIT)
+            {
+                string inputString = Console.ReadLine();
 
-            Commands command = ToCommand(inputString.Trim());
-            Console.WriteLine(command);
+                command = ToCommand(inputString.Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing.";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "This is an open field of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}"; // $...{command} uses the enumeration as part of the string 
+                        break;
+
+                    default:
+                        outputString = "Unrecognized command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
 
         private static Commands ToCommand(string commandString)
