4803f3ebe21247e2318e0fb01f951d45e563ea92
TealTurken <67345081+TealTurken@users.noreply.github.com>
2021-09-08 15:11:31 -0400

    Zork 2.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,6 +4,9 @@ namespace Zork
 {
     class Program
     {
+
+        private static int LocationColumn = 0; // current "Room"
+        // private static string location => Rooms[LocationColumn];
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
@@ -11,6 +14,7 @@ namespace Zork
             Commands command = Commands.UNKNOWN; // assign command a value so it can run the initial comparison
             while (command != Commands.QUIT)
             {
+                Console.WriteLine(">");
                 string inputString = Console.ReadLine();
 
                 command = ToCommand(inputString.Trim());
@@ -22,14 +26,17 @@ namespace Zork
                         outputString = "Thank you for playing.";
                         break;
                     case Commands.LOOK:
-                        outputString = "This is an open field of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        outputString = $"You are at {Rooms [LocationColumn]}";
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
-                    case Commands.WEST:
-                        outputString = $"You moved {command}"; // $...{command} uses the enumeration as part of the string 
+                    case Commands.WEST: // call on your Move method to determine if inputting a direction command actually succeeds.
+                        if (Move(command)) outputString = $"You moved {command}\n{Rooms [LocationColumn]}"; // $...{command} uses the enumeration as part of the string
+
+                        else outputString = "The way is shut!"; // if Move method returns a false, inform the player the path is shut.
+
                         break;
 
                     default:
@@ -41,9 +48,30 @@ namespace Zork
             }
         }
 
-        private static Commands ToCommand(string commandString)
+        private static bool Move(Commands command) // A "Move" method to move the player, called upon by Main to determine if the player can actually move.
         {
-            return Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+            bool didMove = false;
+
+            switch (command)
+            {
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                    break;
+
+                case Commands.EAST when LocationColumn < Rooms.Length - 1:
+                LocationColumn++;
+                didMove = true;
+                    break;
+
+                case Commands.WEST when LocationColumn > 0:
+                LocationColumn--;
+                didMove = true;
+                    break;
+            }
+            return didMove;
         }
+        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        
+        private static string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" }; // You want this available outside Main, hence outside Main as private static
     }
 }
